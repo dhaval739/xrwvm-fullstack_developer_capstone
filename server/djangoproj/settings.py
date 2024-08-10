3e3c29322b4cd98dd3174dd49a5fef0eea00deac
Dhaval <dhavalbhadresha@gmail.com>
2024-08-10 09:17:58 -0400

    Build CarModel and CarMake Django Models

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,11 +33,11 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'dhavalbhadre-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'dhavalbhadre-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://dhavalbhadre-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+    'https://dhavalbhadre-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 CSRF_COOKIE_SECURE = False
 REST_FRAMEWORK = {
